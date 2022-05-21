5a20d68d88015a7af8d33fefd82395a63c708bf5
eric <naber2504@gmail.com>
2022-05-22 01:16:03 +0200

    Edited main.py

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ def read_points(file_name="points"):
         with open(file_name) as file:
             data = file.readlines()
             for line in data:
-                if re.search(r"^\s*#", line):
+                if re.search(r"^\s*#", line) or line.strip() == "":
                     continue
                 tmp = re.findall(r"\d+", line)[1:]
-                dots.append({'origin': (tmp[0], tmp[1]), 'degree': tmp[2], 'lenght': tmp[3]})
+                dots.append({'origin': (tmp[0], tmp[1]), 'degree': tmp[2], 'length': tmp[3]})
     except FileNotFoundError as exc:
-        print(exc)
+        print(f"An Error occurred: {exc}")
+    except BaseException as exc:
+        print(f"An Error occurred: {exc}")
     return dots
+
+
+def print_point(point_):
+    pass
 
 
 def main():
     dots = read_points()
+    print(dots)
 
 
 if __name__ == "__main__":
